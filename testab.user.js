c74839e25e9bcb413d8687f2c8bf8b5aa12fb0ee
git-s80 <84095624+git-s80@users.noreply.github.com>
2021-05-12 19:38:21 +0200

    Create testab.user.js

---
+++ testab.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Test AB2
+// @namespace    http://testab.si/
+// @version      0.1
+// @description  test AB2
+// @author       You
+// @match        https://www.avanture.net/*
+// @match        https://avanture.net/*
+// @match        https://www.sloescort.com/*
+// @match        http://www.oglasi.si/oglas_zasebni_stiki/*
+// @match        http://www.oglasi.si/zasebni_stiki/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @require      https://code.jquery.com/jquery-3.5.1.min.js
+// ==/UserScript==
+
+var site = "";
+
+
+(function() {
+alert('a');
+})();
+
