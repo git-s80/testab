dd9169cc956290e5f732f572818cd96986ce4785
git-s80 <84095624+git-s80@users.noreply.github.com>
2021-05-13 18:46:18 +0200

    Update testabview.user.js

--- testabview.user.js
+++ testabview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Test AB view
 // @namespace    http://view.testab.si/
-// @version      0.26
+// @version      0.27
 // @description  test AB view
 // @author       You
 // @match        https://www.avanture.net/*
@@ -35,18 +35,40 @@ var site = "";
 
 function setupA() {
     var citiesX = [];
+    var agesX = [];
     $("#infinitescroll-classifieds-container > .content-item").each(function(i){
         citiesX.push($(".location strong", this).text());
     });
-    var cities = xUnique(citiesX);
+    $("#infinitescroll-classifieds-container > .content-item").each(function(i){
+        agesX.push($(".age strong", this).text());
+    });
+    var cities = xUnique(citiesX).sort();
+    var ages = xUnique(agesX).sort();
+
+    var div0 = document.createElement('div');
+    var divC = document.createElement('div');
+    var divA = document.createElement('div');
+    $(div0).insertAfter($("div#content > div.content-header > h1"));
+
+    $(divC).appendTo($(div0)).addClass("siteAcities");
+    $(divA).appendTo($(div0)).addClass("siteAages");
 
     $(cities).each(function() {
         var city=this;
         var span = document.createElement('span');
         span.appendChild(document.createTextNode(city));
-        $(span).on("click", function(){siteAshowOnlyCity(city)});
-        $(span).addClass("siteAshowOnly");
-        $(span).insertAfter($("div#content > div.content-header > h1"));
+        $(span).on("click", function(){siteAshowOnlyCity(city, this)});
+        $(span).addClass("siteAshowOnly").addClass("siteAshowOnlyCity");
+        $(span).appendTo($(divC));
+    });
+
+    $(ages).each(function() {
+        var age=this;
+        var span = document.createElement('span');
+        span.appendChild(document.createTextNode(age));
+        $(span).on("click", function(){siteAshowOnlyAge(age, this)});
+        $(span).addClass("siteAshowOnly").addClass("siteAshowOnlyAge");
+        $(span).appendTo($(divA));
     });
 
     $("div#content > div.content-header > h1").on("click", function(){siteAshowAll()});
@@ -59,12 +81,27 @@ function xUnique(array) {
 }
 
 function siteAshowAll() {
+    $(".siteAshowOnly ").removeClass("siteAselected");
     $("#infinitescroll-classifieds-container > .content-item").each(function(i){
         $(this).show();
     });
 }
 
-function siteAshowOnlyCity(city) {
+function siteAshowOnlyAge(age, sender) {
+    $(".siteAshowOnly ").removeClass("siteAselected");
+    $(sender).addClass("siteAselected");
+    $("#infinitescroll-classifieds-container > .content-item").each(function(i){
+        if ($(".age strong", this).text() != age){
+            $(this).hide();
+        } else {
+            $(this).show();
+        }
+    });
+}
+
+function siteAshowOnlyCity(city, sender) {
+    $(".siteAshowOnly ").removeClass("siteAselected");
+    $(sender).addClass("siteAselected");
     $("#infinitescroll-classifieds-container > .content-item").each(function(i){
         if ($(".location strong", this).text().toLowerCase() != city.toLowerCase()){
             $(this).hide();
